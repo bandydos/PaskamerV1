38b7885454560ddd6c5c49085729832820a3a2ff
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-04-25 20:14:00 +0200

    Added some exception handling

--- scripts/script.js
+++ scripts/script.js
@@ -76,27 +76,33 @@ function generateMaleWeightCategories() {
     var person = getPerson();
     var weight = person.weight;
     var weigthCategory;
-    if (weight < 50) {
-        weigthCategory = weightCategories[0];
+    if (person != false) {
+        if (weight < 50) {
+            weigthCategory = weightCategories[0];
+        }
+        else if (weight > 50 && weight < 60) {
+            weigthCategory = weightCategories[1];
+        }
+        else if (weight > 60 && weight < 70) {
+            weigthCategory = weightCategories[2];
+        }
+        else if (weight > 70 && weight < 80) {
+            weigthCategory = weightCategories[3];
+        }
+        else if (weight > 80 && weight < 90) {
+            weigthCategory = weightCategories[4];
+        }
+        else if (weight > 90 && weight < 100) {
+            weigthCategory = weightCategories[5];
+        }
+        else if (weight > 100) {
+            weigthCategory = weightCategories[6];
+        }
     }
-    else if (weight > 50 && weight < 60) {
-        weigthCategory = weightCategories[1];
+    else {
+        weigthCategory = false;
     }
-    else if (weight > 60 && weight < 70) {
-        weigthCategory = weightCategories[2];
-    }
-    else if (weight > 70 && weight < 80) {
-        weigthCategory = weightCategories[3];
-    }
-    else if (weight > 80 && weight < 90) {
-        weigthCategory = weightCategories[4];
-    }
-    else if (weight > 90 && weight < 100) {
-        weigthCategory = weightCategories[5];
-    }
-    else if (weight > 100) {
-        weigthCategory = weightCategories[6];
-    }
+
     return weigthCategory;
 }
 
@@ -110,45 +116,51 @@ function generateMaleHeightCategories() {
     var person = getPerson();
     var height = person.height;
     var heigthCategory;
-    if (height < 100) {
-        heigthCategory = heightCategories[0];
+    if (person != false) {
+        if (height < 100) {
+            heigthCategory = heightCategories[0];
+        }
+        else if (height > 100 && height < 110) {
+            heigthCategory = heightCategories[1];
+        }
+        else if (height > 110 && height < 120) {
+            heigthCategory = heightCategories[2];
+        }
+        else if (height > 120 && height < 130) {
+            heigthCategory = heightCategories[3];
+        }
+        else if (height > 130 && height < 140) {
+            heigthCategory = heightCategories[4];
+        }
+        else if (height > 140 && height < 150) {
+            heigthCategory = heightCategories[5];
+        }
+        else if (height > 150 && height < 160) {
+            heigthCategory = heightCategories[6];
+        }
+        else if (height > 160 && height < 170) {
+            heigthCategory = heightCategories[7];
+        }
+        else if (height > 170 && height < 180) {
+            heigthCategory = heightCategories[8];
+        }
+        else if (height > 180 && height < 190) {
+            heigthCategory = heightCategories[9];
+        }
+        else if (height > 190 && height < 200) {
+            heigthCategory = heightCategories[10];
+        }
+        else if (height > 200 && height < 210) {
+            heigthCategory = heightCategories[11];
+        }
+        else if (height > 210) {
+            heigthCategory = heightCategories[12];
+        }
     }
-    else if (height > 100 && height < 110) {
-        heigthCategory = heightCategories[1];
+    else {
+        heigthCategory = false;
     }
-    else if (height > 110 && height < 120) {
-        heigthCategory = heightCategories[2];
-    }
-    else if (height > 120 && height < 130) {
-        heigthCategory = heightCategories[3];
-    }
-    else if (height > 130 && height < 140) {
-        heigthCategory = heightCategories[4];
-    }
-    else if (height > 140 && height < 150) {
-        heigthCategory = heightCategories[5];
-    }
-    else if (height > 150 && height < 160) {
-        heigthCategory = heightCategories[6];
-    } 
-    else if (height > 160 && height < 170) {
-        heigthCategory = heightCategories[7];
-    } 
-    else if (height > 170 && height < 180) {
-        heigthCategory = heightCategories[8];
-    } 
-    else if (height > 180 && height < 190) {
-        heigthCategory = heightCategories[9];
-    }
-    else if (height > 190 && height < 200) {
-        heigthCategory = heightCategories[10];
-    }
-    else if (height > 200 && height < 210) {
-        heigthCategory = heightCategories[11];
-    }
-    else if (height > 210) {
-        heigthCategory = heightCategories[12];
-    }
+    
     return heigthCategory;
 }
 
@@ -156,7 +168,6 @@ var sizes = ["xs", "s", "m", "l", "xl"];
 
 //Generate sizes for m.
 function generateMaleSizes() {
-    var person = getPerson();
     var weightCategory = generateMaleWeightCategories();
     var heightCategory = generateMaleHeightCategories();
     var size;
@@ -185,13 +196,17 @@ var btnGenerate = document.getElementById('btn-generate');
 btnGenerate.onclick = function () {
     var pSize = document.getElementById('p-size');
     var person = getPerson();
-    if (person.gender = "m") {
-        pSize.textContent = generateMaleSizes();
-    }
-    else if (person.gender = "f") {
-        pSize.textContent = generateFemaleSizes();
+    var size;
+    if (person != false) {
+        if (person.gender = "m") {
+            size = generateMaleSizes();
+        }
+        else if (person.gender = "f") {
+            size = generateFemaleSizes();
+        }
     }
     else {
-        pSize.textContent = "Please enter all fields correctly.";
+        size = "Please fill in all fields correctly.";
     }
+    pSize.textContent = size;
 }
